6a88972f5dd9eca9001b353a5a9cc62ca3bca66e
Marcel Rico <personal.mr4@gmail.com>
2019-12-01 17:51:38 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -3,29 +3,30 @@ require 'directors_database'
 require 'pp'
 
 def directors_totals(nds)
+  # binding.pry
+# {directorName => earnings ...}
+  counter = 0
+  new_hash = {}
+  director_names = []
 
-  r_hash = {}
-  
-  # Begin Traversal of the NDS by traversing into the Outer Array via Outer Index (Responsible for selecting different Directors)
-  directors_database.length.times do |outer_idx|
+  while counter < nds.length do
+    total_gross = 0
+    name = nds[counter][:name]
+    movies = nds[counter][:movies]
+    index = 0
 
-  # Initialize Variables for Directors Name and Grand Total of Worldwide Gross Profits
-    directors_name = ""
-    grand_total = 0
-    
-  # Store Current Directors name in a variable
-    directors_name = directors_database[outer_idx][:name]
-    selected_movies = directors_database[outer_idx][:movies]
-    
-  # Begin Traversal of the Inner NDS by traversing into the inner movies array via Inner Index 
-  # (Responsible for selecting different Movies from specified Director)
-    selected_movies.length.times do |inner_idx|
-      # Add current movie's Worldwide Gross value to the value of grand_total and store in grand_total variable.
-      grand_total += selected_movies[inner_idx][:worldwide_gross]
+    while index < movies.length do
+      directors_earnings = movies[index][:worldwide_gross]
+      total_gross += directors_earnings
+      index += 1
     end
-    
-  # Create a key,value pair in the hash r_hash in preperation for returning this data out of the method.
-    r_hash[directors_name] = grand_total
+
+    new_hash[name] = total_gross
+    # Store directors earnings
+    counter += 1
   end
-  r_hash
+  # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
+  new_hash
 end
+
+
