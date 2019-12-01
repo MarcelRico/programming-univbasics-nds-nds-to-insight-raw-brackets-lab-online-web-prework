e3ca4b9c3c14c6bbf71f10c9ae4573f4553ebc83
Marcel Rico <personal.mr4@gmail.com>
2019-12-01 17:52:04 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -2,31 +2,6 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 require 'pp'
 
-def directors_totals(nds)
-  # binding.pry
-# {directorName => earnings ...}
-  counter = 0
-  new_hash = {}
-  director_names = []
 
-  while counter < nds.length do
-    total_gross = 0
-    name = nds[counter][:name]
-    movies = nds[counter][:movies]
-    index = 0
-
-    while index < movies.length do
-      directors_earnings = movies[index][:worldwide_gross]
-      total_gross += directors_earnings
-      index += 1
-    end
-
-    new_hash[name] = total_gross
-    # Store directors earnings
-    counter += 1
-  end
-  # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
-  new_hash
-end
 
 
